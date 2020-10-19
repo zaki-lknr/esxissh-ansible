ab31c71f484eb2c9d8e45f6d64da921642656c19
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-10-19 22:38:34 +0900

    add: esxisshのパッケージ化(pip install対応)

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='esxissh',
+    version='0.0.1',
+    description='Python Library for ESXi on ssh',
+    author='zaki',
+    author_email='zaki.hmkc+github@gmail.com',
+    url='https://github.com/zaki-lknr',
+    packages=setuptools.find_packages(),
+)
